be5570da697b9542beff5ab2a2f42955fd6e9e77
Pluser761 <danchik761@yandex.ru>
2019-07-13 19:25:54 +0300

    сохранение перед переходом на чтение ios

--- HaphmannLib.h
+++ HaphmannLib.h
@@ -8,8 +8,8 @@ using namespace std;
 class tree
 {
 private:
-	char letter;
-	int quantity;
+	char letter;  // данные
+	int quantity; // 
 public:
 	tree *first;
 	tree *second;
@@ -18,6 +18,43 @@ public:
 	{
 
 	}
+	tree(string file_name)
+	{
+		char *st = new char[file_name.length() + 1];
+
+		for (int i = 0; i < file_name.length(); i++)
+			st[i] = file_name[i];
+		st[file_name.length()] = '\0';
+
+		ifstream fin(st, ifstream::in | ifstream::binary);
+		list<tree> letters; //алфавит-список
+		char c;
+
+		for (fin >> c; !fin.eof(); fin >> c) //создание алфавита-списка из файла
+		{
+			cout << c << endl;
+			list<tree>::iterator finder = find(letters.begin(), letters.end(), tree(c));
+			if (finder == letters.end())
+				letters.push_back(tree(c));
+			else
+				(*finder)++;
+		}
+
+		while (letters.size() != 1) //проход по алфавиту-списку до единственного искомого tree
+		{
+			letters.sort([](tree &first, tree &second) -> bool
+			{
+				return (first.get_quantity() < second.get_quantity());
+			});
+
+			tree inserter(next(letters.begin(), 0), next(letters.begin(), 1));
+			letters.pop_front();
+			letters.pop_front();
+			letters.push_front(inserter);
+		}
+
+		*this = letters.front();
+	} // ***********
 	tree(char l)
 	{
 		letter = l;
@@ -59,7 +96,9 @@ public:
 			second = new tree(t2->letter, t2->quantity);
 	}
 	~tree()
-	{}
+	{
+
+	}
 
 	void operator++(int)
 	{
@@ -90,19 +129,19 @@ public:
 		if (quantity == 0) return first->get_quantity() + second->get_quantity();
 		else return this->quantity;
 	}
-
-	map<int, char> codes_collection()
+	void codes_collection(map<char, string> &ret, string s)
 	{
-		
+		if (quantity == 0)
+		{
+			first->codes_collection(ret, s + '0');
+			second->codes_collection(ret, s + '1');
+		}
+		else
+		{
+			ret.insert(pair<char, string>(this->letter, s));
+		}
 	}
 };
-
-
-
-void writer()
-{
-	
-}
 
 tree get_tree(string s)
 {
@@ -112,17 +151,17 @@ tree get_tree(string s)
 		st[i] = s[i];
 	st[s.length()] = '\0';
 
-	ifstream fin(st, ifstream::in);
-	ofstream fout("Text.txt", ofstream::out);
+	ifstream fin(st, ifstream::in | ifstream::binary);
 	list<tree> letters; //алфавит
-	char c;
+	char byte;
+	fin.read(&byte, sizeof(char));
 
-	for (fin >> c; !fin.eof(); fin >> c)
+	for (int i = 0; i < sizeof(fin); i++, fin.read(&byte, sizeof(char)))
 	{
-		//fout << c;
-		list<tree>::iterator finder = find(letters.begin(), letters.end(), tree(c));
+		cout << byte << endl;
+		list<tree>::iterator finder = find(letters.begin(), letters.end(), tree(byte));
 		if (finder == letters.end())
-			letters.push_back(tree(c));
+			letters.push_back(tree(byte));
 		else
 			(*finder)++;
 	}
@@ -142,9 +181,7 @@ tree get_tree(string s)
 	
 	//output letters
 
-	cout << letters.front();
 
-	return tree();
+	return letters.front();
 }
-
 
--- Source.cpp
+++ Source.cpp
@@ -2,15 +2,92 @@
 #include <fstream>
 #include <string>
 #include "HaphmannLib.h"
+#include <windows.h>
+#include <locale>
 
 using namespace std;
+
+//int main()
+//{
+//	string s = "file.pdf";
+//
+//	ifstream fin(s, ifstream::in | ifstream::binary);
+//	ofstream fout("archive_" + s.substr(0, s.length() - 3) + "arc", ofstream::out);
+//
+//	char c;
+//	fin >> c;
+//	for ( int i = 0; i < sizeof(fin); i++, fin >> c)
+//	{
+//		fout << c << " | ";
+//	}
+//
+//	fin.close();
+//
+//	system("pause");
+//	return 0;
+//}
+
+
 
 int main()
 {
 	string s = "file.pdf";
+	map<char, string> coll;
 
-	get_tree(s);
+	//get_tree(s).codes_collection(coll, "1");
+	tree(s).codes_collection(coll, "1");
 
-	system("pause");
+	fstream fin(s, ifstream::in | ifstream::binary);
+	fstream fout("archive_" + s.substr(0, s.length() - 3) + "arc", ofstream::out);
+
+	fout << s.substr(5, s.length()) << endl; //формат файла
+	for (auto it = coll.begin(); it != coll.end(); ++it) //дерево с разделителем :
+		fout << it->first << ":" << it->second << endl;
+	fout << "%%%" << endl; //конец алфавита
+
+	char byte, c;
+	fin.read(&byte, sizeof(char));
+	for ( int i = 0; i < sizeof(fin)/ sizeof(char); i++, fin.read(&byte, sizeof(char))) //чтение всех байтов fin
+		fout << coll.find((char)byte)->second << "_"; //поиск файла в коллекции map
+
+	coll.clear();
+	fin.close();
+	fout.close();
+
+
+	return 0;
+
+
+
+	map<string, char> uncoll;
+	fin.open("archive_" + s.substr(0, s.length() - 3) + "arc", ifstream::in);
+	getline(fin, s);
+	fout.open("unarchived." + s, ofstream::out | ofstream::binary);
+
+	for (fin >> s; s != "%%%"; fin >> s)//воссоздание дерева
+	{
+		cout << s << endl;
+		uncoll.insert(pair<string, char>(s.substr(2, s.length()), s[0]));
+		//system("pause");
+	}
+	
+	string temp = "";
+	for (fin >> c; !fin.eof(); fin >> c)
+	{
+		if (c != '_')
+		{
+			temp += c;
+		}
+		else
+		{
+			fout << uncoll.find(temp)->second;
+			temp = "";
+		}
+	}
+
+	fin.close();
+	fout.close();
+
+	//system("pause");
 	return 0;
 }
